04ed15676d15b446f09e00b039b00a1bbd983d9e
CodeNMore <jackofall55@gmail.com>
2023-05-07 15:27:19 -0400

    Removed Any number check

--- pkg/apc/any.go
+++ pkg/apc/any.go
@@ -3,10 +3,6 @@ package apc
 // Returns a parser that attempts to parse, in order, the provided parsers.
 // Returns the result of the first successful parser.
 func Any[CT, T any](parsers ...Parser[CT, T]) Parser[CT, T] {
-	if len(parsers) < 2 {
-		panic("must provide at least 2 parsers to Any")
-	}
-
 	return func(ctx Context[CT]) (T, error) {
 		for _, parser := range parsers {
 			node, err := parser(ctx)
