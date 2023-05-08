6cc4869f3f3a1ab13a219c74e1d380b9d2fa4c9f
CodeNMore <jackofall55@gmail.com>
2023-05-07 23:11:18 -0400

    more build options

--- pkg/apcgen/build_options.go
+++ pkg/apcgen/build_options.go
@@ -2,6 +2,7 @@ package apcgen
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/tpillow/apc/pkg/apc"
 )
@@ -21,11 +22,32 @@ func WithDefaultBuildOptions[CT any](buildFuncs ...BuildOptionFunc[CT]) *BuildOp
 	}
 	return opts
 }
+
 func WithParserOption[CT any](name string, parser apc.Parser[CT, any]) BuildOptionFunc[CT] {
 	return func(opts *BuildOptions[CT]) {
 		if _, has := opts.ProvidedParsers[name]; has {
 			panic(fmt.Sprintf("cannot use WithParserOption: name '%v' already specified", name))
 		}
 		opts.ProvidedParsers[name] = parser
+	}
+}
+
+func WithBuildParser[RT any](skipWhitespace bool) BuildOptionFunc[rune] {
+	return func(opts *BuildOptions[rune]) {
+		typeName := reflect.TypeOf(new(RT)).Elem().Name()
+		var parser *apc.Parser[rune, *RT]
+		parserRef := apc.Ref(parser)
+		WithParserOption(typeName, apc.CastToAny(parserRef))
+		*parser = BuildParser[RT](opts, skipWhitespace)
+	}
+}
+
+func WithBuildTokenizedParser[RT any]() BuildOptionFunc[apc.Token] {
+	return func(opts *BuildOptions[apc.Token]) {
+		typeName := reflect.TypeOf(new(RT)).Elem().Name()
+		var parser *apc.Parser[apc.Token, *RT]
+		parserRef := apc.Ref(parser)
+		WithParserOption(typeName, apc.CastToAny(parserRef))
+		*parser = BuildTokenizedParser[RT](opts)
 	}
 }
