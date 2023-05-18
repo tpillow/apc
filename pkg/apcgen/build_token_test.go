3545ad199d6e69faaaba41ba0464b44dd11fb6f5
CodeNMore <jackofall55@gmail.com>
2023-05-18 14:06:03 -0400

    Added test case

--- pkg/apcgen/build_token_test.go
+++ pkg/apcgen/build_token_test.go
@@ -7,6 +7,48 @@ import (
 	"github.com/tpillow/apc/pkg/apc"
 )
 
+func TestTokenParserBasic1(t *testing.T) {
+	type AssignExpr struct {
+		Name string `apc:"look($'Ident' token('Ident', '='))"`
+		Expr string `apc:"$'Ident' token('Ident', ';')"`
+	}
+	type TypeExpr struct {
+		Name string `apc:"look($'Ident' token('Ident', 'type'))"`
+		_    string `apc:"token('Ident', ';')"`
+	}
+	type Result struct {
+		Things []any `apc:"$(AssignExpr | TypeExpr)*"`
+	}
+
+	var TokenTypeIdent apc.TokenType = "Ident"
+	lexParser := apc.Skip(
+		apc.CastToAny(apc.WhitespaceParser),
+		apc.Any(
+			apc.BindToToken(apc.Regex("[^\\s]+"), TokenTypeIdent),
+		),
+	)
+
+	parser := BuildTokenizedParser[Result](WithDefaultBuildOptions(
+		WithBuildTokenizedParserOption[AssignExpr](),
+		WithBuildTokenizedParserOption[TypeExpr](),
+	))
+
+	ctx := apc.NewStringContext(testOriginName, `name = value ; some type ; another type ; name2 = hahaha ;`)
+	lexer := apc.NewParseReader[rune](ctx, lexParser)
+	lexerCtx := apc.NewReaderContext[apc.Token](lexer)
+
+	node, err := apc.Parse[apc.Token](lexerCtx, parser, apc.DefaultParseConfig)
+	assert.NoError(t, err)
+	assert.Equal(t, Result{
+		Things: []any{
+			AssignExpr{Name: "name", Expr: "value"},
+			TypeExpr{Name: "some"},
+			TypeExpr{Name: "another"},
+			AssignExpr{Name: "name2", Expr: "hahaha"},
+		},
+	}, node)
+}
+
 func TestTokenParserCaptureBasic(t *testing.T) {
 	type Person struct {
 		Name string `apc:"'Ident:person' $'Ident'"`
